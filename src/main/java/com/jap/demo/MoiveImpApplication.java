ccce857194f98b612fcd11a0142eba668d52d56d
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:19:19 +0530

    initial commit

---
+++ src/main/java/com/jap/demo/MoiveImpApplication.java
@@ -0,0 +1,13 @@
+package com.jap.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiveImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiveImpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jap/demo/MoiveImpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jap.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiveImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
