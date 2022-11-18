78eeed8ba309cef13365fa833d7d9e66beac1eff
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:34:43 +0530

    added MovieService object and initialized using parametrized

--- src/main/java/com/jap/demo/controller/MovieController.java
+++ src/main/java/com/jap/demo/controller/MovieController.java
@@ -1,4 +1,15 @@
 package com.jap.demo.controller;
 
+import com.jap.demo.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieController {
+    private MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 }
