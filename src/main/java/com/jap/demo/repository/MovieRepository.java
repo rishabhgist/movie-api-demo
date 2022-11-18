a8de56557fb03e217583da8f35c854ab63db54cf
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:27:13 +0530

    added CrudRepository interface in MovieRepository

--- src/main/java/com/jap/demo/repository/MovieRepository.java
+++ src/main/java/com/jap/demo/repository/MovieRepository.java
@@ -1,4 +1,9 @@
 package com.jap.demo.repository;
 
-public class MovieRepository {
+import com.jap.demo.domain.Movie;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
 }
