d38158753912fb4ad4cc84ec6b8813a2e1c61135
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:29:55 +0530

    added MovieService class implements MovieService Interface

---
+++ src/main/java/com/jap/demo/service/MovieService.java
@@ -0,0 +1,17 @@
+package com.jap.demo.service;
+
+import com.jap.demo.domain.Movie;
+
+import java.util.List;
+
+public class MovieService implements IMoiveService{
+    @Override
+    public Movie save(Movie movie) {
+        return null;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        return null;d
+    }
+}
