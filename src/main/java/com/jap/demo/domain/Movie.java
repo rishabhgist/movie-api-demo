66dc73272e825dae33497aa3f8ae55abc454bd55
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:25:02 +0530

    added fields and getter and setters and constructor

--- src/main/java/com/jap/demo/domain/Movie.java
+++ src/main/java/com/jap/demo/domain/Movie.java
@@ -1,4 +1,55 @@
 package com.jap.demo.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Movie {
+    @Id
+    private int id;
+    private String name;
+    private String actor;
+    private String director;
+
+    public Movie() {
+    }
+
+    public Movie(int id, String name, String actor, String director) {
+        this.id = id;
+        this.name = name;
+        this.actor = actor;
+        this.director = director;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getActor() {
+        return actor;
+    }
+
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
