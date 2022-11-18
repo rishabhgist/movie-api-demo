6ea55aa826a0e0e5beec0de5c947d36b1eb4a86e
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:32:10 +0530

    added MovieRepository object and used in save and getAll method

--- src/main/java/com/jap/demo/service/MovieService.java
+++ src/main/java/com/jap/demo/service/MovieService.java
@@ -1,17 +1,28 @@
 package com.jap.demo.service;
 
 import com.jap.demo.domain.Movie;
+import com.jap.demo.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public class MovieService implements IMoiveService{
+@Service
+public class MovieService implements IMoiveService {
+    private final MovieRepository movieRepository;
+
+    @Autowired
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
     @Override
     public Movie save(Movie movie) {
-        return null;
+        return movieRepository.save(movie);
     }
 
     @Override
     public List<Movie> getAll() {
-        return null;d
+        return (List<Movie>) movieRepository.findAll();
     }
 }
