82e5f2432676910647e01dfd52f82d18d3826018
Rishabh Gist <damyrachit@gmail.com>
2022-11-18 15:40:58 +0530

    added saveMovie and getMovie method

--- src/main/java/com/jap/demo/controller/MovieController.java
+++ src/main/java/com/jap/demo/controller/MovieController.java
@@ -1,8 +1,11 @@
 package com.jap.demo.controller;
-
+import com.jap.demo.domain.Movie;
 import com.jap.demo.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovieController {
@@ -11,5 +14,13 @@ public class MovieController {
     @Autowired
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
+    }
+    @PostMapping("/save")
+    public ResponseEntity<?> saveMovie(@RequestBody Movie movie){
+        return new ResponseEntity<>(movieService.save(movie), HttpStatus.CREATED);
+    }
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getMovie(){
+        return new ResponseEntity<>(movieService.getAll(), HttpStatus.OK);
     }
 }
